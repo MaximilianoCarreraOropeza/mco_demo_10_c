dee0c47a727ad2f80d229d365754d1a72369e2a1
Maximiliano <20223tn012@utez.edu.mx>
2025-10-22 19:52:03 -0600

    [FIRST COMMIT] SUBIDA INICIAL AL REPOSITORIO DEL PROYECTO

---
+++ client/src/modules/test/test.controller.js
@@ -0,0 +1,20 @@
+const TestController = {}
+const ENV = import.meta.env;
+
+const API_URL = `http://${ENV.VITE_API_HOST}:${ENV.VITE_API_PORT}${ENV.VITE_API_BASE}`
+
+TestController.callToAPI = async () => {
+    await fetch(`${API_URL}/test`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    .then(res => res.json())
+    .then(
+        result => console.log(result)
+    )
+    .catch(console.error)
+}
+
+export default TestController;
